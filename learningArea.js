2c2095beb6078ecac5ee0169831d6781e963c2c6
daniel keane <20098745@mail.wit.ie>
2025-02-10 13:50:28 +0000

    V0.1 Creating basic pages for extension

---
+++ learningArea.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('returnMain').addEventListener('click', function() {
+        window.location.href = 'main.html';
+    });
+});
